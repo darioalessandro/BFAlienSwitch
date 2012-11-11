593a2b651c0589dfd73ee969baee6d3445452f48
Dario Lencina <darioalessandro@me.com>
2012-11-10 22:21:37 -0500

    Initialized repo

---
+++ BFAlienSwitch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BFAlienSwitch
+//
+//  Created by Dario Lencina on 11/10/12.
+//  Copyright (c) 2012 Dario Lencina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
